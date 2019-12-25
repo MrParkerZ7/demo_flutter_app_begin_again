9fd0aa72a85ce437d04b9c67bbb732d68e3bae55
Promsawat <Neverzero9@gmail.com>
2019-12-25 14:21:29 +0700

    [More]

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,56 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class DemoRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo navigate page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class DemoListViewBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo navigate page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  color: Colors.amber[colorCodes[index]],
+                  child: Center(child: Text('Entry ${entries[index]}')),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -56,6 +106,24 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 50,
             color: Colors.amber[100],
             child: const Center(child: Text('Entry C')),
+          ),
+          RaisedButton(
+            child: Text('Demo route'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DemoRoute()),
+              );
+            },
+          ),
+          RaisedButton(
+            child: Text('Demo list view builder'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DemoListViewBuilder()),
+              );
+            },
           ),
         ],
       ),
